397d0a6c94b3c5cd22d72ab6531d1d150798567d
Leo <leo@LeodeMacBook-Pro.local>
2026-02-26 14:17:48 +0800

    style: 小重構

--- rabbitmq/conn.go
+++ rabbitmq/conn.go
@@ -30,30 +30,23 @@ type Config struct {
 func SetConn(config Config) {
 	var err error
 
-	url := fmt.Sprintf(
-		"amqp://%s:%s@%s:%s/%s",
-		config.User,
-		config.Password,
-		config.Host,
-		config.Port,
-		config.Vhost,
+	conn, err = amqp.DialConfig(
+		buildUrl(config),
+		amqp.Config{
+			Heartbeat: 10 * time.Second, // 由 golang 每 n 秒確認一次 broker 是否存活
+			Locale:    "en_US",
+			Properties: amqp.Table{
+				"connection_name": config.ServiceName, // 該次連線是由哪個服務所建立的
+			},
+			Dial: func(network, addr string) (net.Conn, error) {
+				d := net.Dialer{
+					Timeout:   5 * time.Second,  // TCP 交握的超時時間
+					KeepAlive: 30 * time.Second, // 由 OS 每 n 秒確認一次連線是否存活
+				}
+				return d.Dial(network, addr)
+			},
+		},
 	)
-	log.Println("url:", url)
-
-	conn, err = amqp.DialConfig(url, amqp.Config{
-		Heartbeat: 10 * time.Second, // 由 golang 每 n 秒確認一次 broker 是否存活
-		Locale:    "en_US",
-		Properties: amqp.Table{
-			"connection_name": config.ServiceName, // 該次連線是由哪個服務所建立的
-		},
-		Dial: func(network, addr string) (net.Conn, error) {
-			d := net.Dialer{
-				Timeout:   5 * time.Second,  // TCP 交握的超時時間
-				KeepAlive: 30 * time.Second, // 由 OS 每 n 秒確認一次連線是否存活
-			}
-			return d.Dial(network, addr)
-		},
-	})
 	if err != nil {
 		log.Println("SetConn failed, err:", err.Error())
 		panic(err)
@@ -72,4 +65,17 @@ func Close() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func buildUrl(config Config) string {
+	url := fmt.Sprintf(
+		"amqp://%s:%s@%s:%s/%s",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Vhost,
+	)
+	log.Println("amqp url:", url)
+	return url
 }
